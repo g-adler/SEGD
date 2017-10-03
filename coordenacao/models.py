a731469df8f616e1bfc9b6fd10da320461adcc8f
g-adler <guiadler@hotmail.com>
2017-10-03 09:55:01 -0300

    Initial commit, starting project SGED - Sistema Gerenciador de Envio de Documentos

---
+++ coordenacao/models.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+LISTA_TIPO_DOC = (
+	('0', 'PDF'),('1','WORD'),)
+
+Class Coordenador(models.Model):
+	matricula = models.CharField(max_length = 30 , primary_key = true )
+	nome = models.CharField(max_length = 200)
+	email = models.CharField(max_length = 100)
+	telefone = models.CharField(max_length =11)
+	curso = models.ForeignKey(Curso , on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.nome
+
+Class Documento(models.Model):
+	coordenador = models.ForeignKey(Coordenador , on_delete=models.CASCADE)
+	titulo = models.CharField(max_length =50)
+	tipo = models.CharField(max_length =1, choices = LISTA_TIPO_DOC, default='0')
+
+ 	def __str__(self):
+		return self.titulo
+
+Class Curso(models.Model):
+	titulo = models.CharField(max_length =50)
+	coordenador = models.ForeignKey(Coordenador , on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.titulo
+
