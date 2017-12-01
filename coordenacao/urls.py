eeca2d66b36b20dd2d564382c088cec7aab5f5a5
g-adler <guiadler@hotmail.com>
2017-12-01 18:29:26 -0200

    hotfixing

--- coordenacao/urls.py
+++ coordenacao/urls.py
@@ -6,6 +6,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     # url(r'^$', views.index, name='index'),
     url(r'^$', auth_views.login, {'template_name': 'coordenacao/login.html'}, name='index'),
-    url(r'^Coordenacao$', views.index, name='pg_coord'),
-    url(r'^Direcao$', views.index, name='pg_direc'),
+    url(r'^Coordenacao$', views.pg_coord, name='pgcoord'),
+    url(r'^Direcao$', views.pg_direc, name='pgdirec'),
     ]
